8829399cdd9a84e75860a1ac81418311dcbaf881
Surya-2811 <84892906+surya-2811@users.noreply.github.com>
2023-12-30 14:41:27 +0530

    insert

---
+++ index.js
@@ -0,0 +1,100 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+class LinkedList {
+  constructor(value) {
+    let newNode = new Node(value);
+    this.head = newNode;
+    this.tail = this.head;
+    this.length = 1;
+  }
+  push(value) {
+    let newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+  pop() {
+    if (!this.head) return undefined;
+    let prev = this.head;
+    let temp = this.head;
+    while (temp.next) {
+      prev = temp;
+      temp = temp.next;
+    }
+    this.tail = prev;
+    this.tail.next = null;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return temp;
+  }
+  unshift(value) {
+    let newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      let temp = this.head;
+      this.head = newNode;
+      this.head.next = temp;
+    }
+    this.length++;
+    return this;
+  }
+  shift() {
+    if (!this.head) return undefined;
+    let temp = this.head;
+    this.head = temp.next;
+    temp.next = null;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return temp;
+  }
+  get(index) {
+    if (index < 0 || index > this.length) return undefined;
+    let temp = this.head;
+    for (let i = 0; i < index; i++) {
+      temp = temp.next;
+    }
+    return temp;
+  }
+  set(index, value) {
+    if (!this.get(index)) return false;
+    let temp = this.get(index);
+    temp.value = value;
+    return true;
+  }
+  insert(index, value) {
+    let newNode = new Node(value);
+    if (index === 0) return this.unshift(value);
+    if (index === this.length) return this.push(value);
+    if (index < 0 || index > this.length) return false;
+    let temp = this.get(index - 1);
+    newNode.next = temp.next;
+    temp.next = newNode;
+    this.length++;
+    return true;
+  }
+}
+
+let newLinkedList = new LinkedList(5);
+console.log(newLinkedList.push(6));
+console.log(newLinkedList.push(7));
+
+console.log(newLinkedList.insert(1, 99));
+console.log(newLinkedList);
