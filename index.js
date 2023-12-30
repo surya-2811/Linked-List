810979c4403dfb6c2f2fc0be8a76e110ab253ffb
Surya-2811 <84892906+surya-2811@users.noreply.github.com>
2023-12-30 15:28:48 +0530

    reverse LL

--- index.js
+++ index.js
@@ -90,11 +90,35 @@ class LinkedList {
     this.length++;
     return true;
   }
+  remove(index) {
+    if (index === 0) return this.shift();
+    if (index === this.length - 1) return this.pop();
+    if (index < 0 || index >= this.length) return undefined;
+    let before = this.get(index - 1);
+    let temp = before.next;
+    before.next = temp.next;
+    temp.next = null;
+    this.length--;
+    return this;
+  }
+  reverse() {
+    let temp = this.head;
+    this.head = this.tail;
+    this.tail = temp;
+    let prev = null;
+    let next = temp.next;
+    for (let i = 0; i < this.length; i++) {
+      next = temp.next;
+      temp.next = prev;
+      prev = temp;
+      temp = next;
+    }
+    return this;
+  }
 }
 
-let newLinkedList = new LinkedList(5);
-console.log(newLinkedList.push(6));
-console.log(newLinkedList.push(7));
-
-console.log(newLinkedList.insert(1, 99));
-console.log(newLinkedList);
+let newLinkedList = new LinkedList(0);
+console.log(newLinkedList.push(1));
+console.log(newLinkedList.push(2));
+console.log(newLinkedList.push(3));
+console.log(newLinkedList.reverse());
