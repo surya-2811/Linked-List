e1a5d7fb2e7afcb1d879be4b3e48a0242d8e3f1a
Surya-2811 <84892906+surya-2811@users.noreply.github.com>
2024-01-13 23:39:10 +0530

    Update DLL.js

--- DLL.js
+++ DLL.js
@@ -67,10 +67,72 @@ class DoublyLinkedList {
     this.length--;
     return temp;
   }
+  get(index) {
+    if (index < 0 || index >= this.length) return undefined;
+    let temp = this.head;
+    if (index < this.length / 2) {
+      for (let i = 0; i < index; i++) {
+        temp = temp.next;
+      }
+    } else {
+      for (let i = this.length - 1; i > index; i--) {
+        temp = temp.next;
+      }
+    }
+    return temp;
+  }
+  set(index, value) {
+    let temp = this.get(index);
+    console.log(temp.value, "here");
+    if (temp) {
+      temp.value = value;
+      return true;
+    }
+    return false;
+  }
+  insert(index, value) {
+    if (index === 0) return this.unshift(value);
+    if (index === this.length) return this.push(value);
+    if (index < 0 || index > this.length) return false;
+    let newNode = new Node(value);
+    let before = this.get(index - 1);
+    let after = before.next;
+    newNode.prev = before;
+    before.next = newNode;
+    after.prev = newNode;
+    newNode.next = after;
+    this.length++;
+    return true;
+  }
+  remove(index) {
+    if (index === 0) return this.shift();
+    if (index === this.length - 1) return this.pop();
+    if (index < 0 || index >= this.length) return false;
+    let temp = this.get(index);
+    console.log(temp, "here");
+    temp.prev.next = temp.next;
+    temp.next.prev = temp.prev;
+    temp.prev = null;
+    temp.next = null;
+    this.length--;
+    return true;
+  }
 }
 
-let doublyLinkedList = new DoublyLinkedList(7);
+let doublyLinkedList = new DoublyLinkedList(0);
+console.log(doublyLinkedList.push(1));
+console.log(doublyLinkedList.push(2));
 console.log(doublyLinkedList.push(3));
-console.log(doublyLinkedList.pop());
-console.log(doublyLinkedList.unshift(7));
-console.log(doublyLinkedList.shift());
+console.log(doublyLinkedList.push(4));
+doublyLinkedList.remove(5);
+console.log(displayValue(doublyLinkedList.head));
+
+function displayValue(head) {
+  let array = [];
+  let currentNode = head;
+  while (currentNode) {
+    array.push(currentNode.value);
+    currentNode = currentNode.next;
+  }
+  return array;
+}
