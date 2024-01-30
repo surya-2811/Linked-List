ff38b9c3f0785da8460efd057f5ab0e66c0aeb7f
Surya-2811 <84892906+surya-2811@users.noreply.github.com>
2024-01-30 15:17:47 +0530

    Binary search trees

---
+++ trees.js
@@ -0,0 +1,61 @@
+console.log("Binary Search Tree");
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+class BST {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(value) {
+    const newNode = new Node(value);
+    if (this.root === null) {
+      this.root = newNode;
+      return this;
+    }
+    let temp = this.root;
+    while (true) {
+      if (newNode.value === temp.value) return undefined;
+      if (newNode.value < temp.value) {
+        if (temp.left === null) {
+          temp.left = newNode;
+          return this;
+        }
+        temp = temp.left;
+      } else {
+        if (temp.right === null) {
+          temp.right = newNode;
+          return this;
+        }
+        temp = temp.right;
+      }
+    }
+  }
+  contains(value) {
+    if (this.root === null) return false;
+    let temp = this.root;
+    while (temp) {
+      if (value < temp.value) {
+        temp = temp.left;
+      } else if (value > temp.value) {
+        temp = temp.right;
+      } else {
+        return true;
+      }
+    }
+    return false;
+  }
+}
+
+const tree = new BST();
+tree.insert(7);
+tree.insert(10);
+tree.insert(15);
+tree.insert(99);
+tree.insert(47);
+console.log(tree.contains(78));
